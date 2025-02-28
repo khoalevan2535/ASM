27245f6626400267b3e5c6bf69303db4876fd41d
khoalevan2535 <khoalevan2535@gmail.com>
2025-02-28 15:30:21 +0700

    a

---
+++ src/main/java/com/poly/demo/VanKhoaApplication.java
@@ -0,0 +1,13 @@
+package com.poly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanKhoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanKhoaApplication.class, args);
+	}
+
+}
